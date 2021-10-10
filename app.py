52a525d29a2de5e54e607204cebe944631b514d1
Edgar Eldy <ed.eldy21@live.com>
2021-10-10 22:08:11 +0200

    This script runs the flask_tutorial application using a development server

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-from flask import Flask
+"""
+This script runs the flask_tutorial application using a development server.
+"""
 
-app = Flask(__name__)
-
-
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
-
+from os import environ
+from flask_tutorial import app
 
 if __name__ == '__main__':
-    app.run()
+    HOST = environ.get('SERVER_HOST', 'localhost')
+    try:
+        PORT = int(environ.get('SERVER_PORT', '5000'))
+    except ValueError:
+        PORT = 5000
+    app.run(HOST, PORT)

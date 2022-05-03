952c3c93229c111eb8f10390bea6206e611afa4e
Edgar Eldy <ed.eldy21@live.com>
2022-05-03 22:14:03 +0300

    Setting up database connection

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+# Setting up database connection
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flask_db'
+db = SQLAlchemy(app)
 
 @app.route('/')
 def hello_world():  # put application's code here
